286040fd139dd9e11678be761c143893c7825d28
josh reese <droptablestar@gmail.com>
2018-09-02 08:41:24 -0500

    Nested routing for messages inside users and channels

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :messages
-  resources :channels
-  resources :users
+  resources :users do
+    resources :messages, only: [:index, :create, :show]
+  end
+  resources :channels do
+    resources :messages, only: [:index]
+    resources :users, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
