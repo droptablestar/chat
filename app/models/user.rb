bb65f7450c0c812502498a67e5056ec7aa8e6b90
josh reese <droptablestar@gmail.com>
2018-09-02 11:20:00 -0500

    Removed previous password stuff cuz devise doesn't need it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   has_many :messages
 
@@ -8,4 +11,16 @@ class User < ApplicationRecord
   # many-to-many with channel
   # has_many :memberships, dependent: :destroy
   # has_many :channels, through: :memberships, source: :channel
+
+  validates :alias, length: { minimum: 2 }, format: { with: /\A[a-zA-Z]+[a-zA-Z0-9]*\z/,
+    message: "only allows letters and numbers" }
+
+  # need to do this bc devise always checks email
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
