1c058f5da077469833820b1eaa6b9020e702980c
josh reese <droptablestar@gmail.com>
2018-09-02 14:24:09 -0500

    Routes for socket and messages create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'memberships/new'
-  get 'memberships/delete'
   devise_for :users
 
   root to: 'channels#index'
@@ -9,7 +7,15 @@ Rails.application.routes.draw do
   resources :channels, only: [:index, :show, :edit, :update, :delete, :new]
   resources :memberships, only: [:update, :destroy]
 
+  post '/messages/:id' => 'messages#create', as: 'messages'
+
   get '/chat/:id' => 'channels#chat', as: 'chat'
+
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+
+  # resources :chatrooms, param: :slug
+  # resources :messages
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
