38febd3d6c45c82400c226076d56631df766c9ac
josh reese <droptablestar@gmail.com>
2018-09-02 11:19:28 -0500

    Added devise with login/registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_141723) do
+ActiveRecord::Schema.define(version: 2018_09_02_155444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,9 +44,15 @@ ActiveRecord::Schema.define(version: 2018_09_02_141723) do
 
   create_table "users", force: :cascade do |t|
     t.string "alias"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.index ["alias"], name: "index_users_on_alias", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "channels", "users"
