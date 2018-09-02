b33bc331d81a79840cc0bcf35f1b9f3fbb8963f3
josh reese <droptablestar@gmail.com>
2018-09-02 14:23:33 -0500

    Removed unnecessary code from messages controller and added create functionality

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,7 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
-
-  # GET /messages
-  # GET /messages.json
-  def index
-    @messages = Message.all
-  end
-
   # GET /messages/1
   # GET /messages/1.json
   def show
-  end
-
-  # GET /messages/new
-
-  def new
-    @message = Message.new
   end
 
   # GET /messages/1/edit
@@ -25,36 +11,17 @@ class MessagesController < ApplicationController
   # POST /users/:user_id/messages
   # POST /users/:user_id/messages.json
   def create
-    user = User.find(params[:user_id])
-    if user
-      @message = Message.new(message_params)
-      respond_to do |format|
-        if @message.save
-          format.html { redirect_to @message, notice: 'Message was successfully created.' }
-          format.json { render :show, status: :created, location: @message }
-        else
-          format.html { render :new }
-          format.json { render json: @message.errors, status: :unprocessable_entity }
-        end
-      end
-    else
-      format.html { render :new }
-      format.json { render json: user.errors, status: :unprocessable_entity }
+    channel = Channel.find_by(id: params[:id])
+    message = Message.new(message_params)
+    message.user = current_user
+    message.channel = channel
+    if message.save
+      ActionCable.server.broadcast 'messages',
+        message: message.content,
+        user: message.user.alias
+      # head :ok
     end
-  end
-
-  # PATCH/PUT /messages/1
-  # PATCH/PUT /messages/1.json
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to chat_url(params[:id])
   end
 
   # DELETE /messages/1
@@ -68,11 +35,6 @@ class MessagesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_message
-      @message = Message.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
       params.require(:message).permit(:content, :user_id, :channel_id)
