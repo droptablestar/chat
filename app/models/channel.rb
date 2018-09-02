b1af118b8a8591dfeffa45054076284c1e4e733f
josh reese <droptablestar@gmail.com>
2018-09-02 08:44:52 -0500

    Set up many-to-many's in channel and user.

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,2 +1,9 @@
 class Channel < ApplicationRecord
+  belongs_to :admin, class_name: 'User'
+
+  has_many :messages
+
+  # many-to-many with user
+  has_many :memberships, dependent: :destroy
+  has_many :users, through: :memberships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :messages
+
+  # many-to-many with channel
+  has_many :memberships, dependent: :destroy
+  has_many :channels, through: :memberships
 end
